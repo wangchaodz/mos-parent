8d510a42cdce30d3d9d2ed6b70301ce5729a6391
wangchao <wangchaodz@gmail.com>
2019-09-27 18:49:10 +0800

    1、注册中心单机版 2、注册中心主备高可用版 3、注册中心用户权限版 4、注册中心多端点检测

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mos-parent
+
+## 注意
+
+### 1、注册中心高可用
+    ①同机器,在hosts中增加IP1、IP2指向IP地址，例如127.0.0.1 master salve
+    ②两个项目的spring.application.name必须相同，eureka.instance.hostname必须不同，否则都会失败
---
+++ server-eureka-slave/src/test/java/cn/com/mos/server/ServerEurekaSlaveApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.com.mos.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServerEurekaSlaveApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
